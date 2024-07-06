a1f9aafbcd24e1852a0bca9ce43d879d2461a8f5
Renat Razumov <renatrazumov@users.noreply.github.com>
2024-07-06 14:05:51 -0400

    Update MainNavigator.js to include the new screens

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -5,16 +5,20 @@ import StartActivityScreen from '../screens/ActivityScreens/StartActivityScreen'
 import ActivityDetailScreen from '../screens/ActivityScreens/ActivityDetailScreen';
 import FeedScreen from '../screens/SocialScreens/FeedScreen';
 import FriendsListScreen from '../screens/SocialScreens/FriendsListScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ActivityScreens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function MainNavigator() {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="StartActivity" component={StartActivityScreen} />
       <Tab.Screen name="ActivityDetail" component={ActivityDetailScreen} />
       <Tab.Screen name="Feed" component={FeedScreen} />
       <Tab.Screen name="FriendsList" component={FriendsListScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
