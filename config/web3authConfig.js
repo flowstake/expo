ba5c02b6f96503b5c89a87d30f057d1cee535a47
Renat Razumov <renatrazumov@users.noreply.github.com>
2024-07-06 11:36:57 -0400

    update web3authConfig

--- config/web3authConfig.js
+++ config/web3authConfig.js
@@ -1,8 +1,9 @@
 // flowstake-expo/config/web3authConfig.js
 import { Web3Auth } from '@web3auth/react-native-sdk';
+import { WEB3AUTH_CLIENT_ID } from '@env';
 
 const web3auth = new Web3Auth({
-  clientId: 'your-client-id', // Get your client ID from Web3auth
+  clientId: WEB3AUTH_CLIENT_ID, // Get your client ID from Web3auth
   network: 'testnet', // Use 'mainnet' for production
 });
 
